42662fb4e47e284185b5f2a576bab73b84ba7dc2
Farid Azmi <muhaziz@deloitte.com>
2025-02-03 13:23:41 +0700

    git init

---
+++ src/test/java/com/imza/porto/virtualthread/VirtualThreadDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imza.porto.virtualthread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
